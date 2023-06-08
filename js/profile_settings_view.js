11601183d1bfbdaaadf4fcc5dc31a188f087ba31
Provodov Danylo <provodov.danylo@lll.kpi.ua>
2023-06-08 19:29:38 +0200

    profile_settings_view.js was deleted

--- js/profile_settings_view.js
+++
@@ -1,9 +0,0 @@
-
-export class ProfileSettingsView {
-    constructor() { }
-
-    setButtonView(button, text, color) {
-        button.textContent      = text;
-        button.style.background = color;
-    }
-}
